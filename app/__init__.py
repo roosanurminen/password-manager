86fc5d9858443afd3896e06d31325944f6d18193
Roosa Nurminen <roosa.nurminen7@gmail.com>
2026-03-05 12:21:14 +0200

    Fix routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+import time
+from sqlalchemy.exc import OperationalError
 
 # Configure SQL Alchemy
 db = SQLAlchemy()
@@ -18,7 +20,20 @@ def create_app():
     from .routes import main
     app.register_blueprint(main)
 
-    with app.app_context():
-        db.create_all()
+    max_retries = 10
+    for i in range(max_retries):
+        try:
+            with app.app_context():
+                db.create_all()
+            print("Database connected successfully!")
+            break
+        except OperationalError:
+            print(f"Database not ready, retrying ({i+1}/{max_retries})...")
+            time.sleep(2)
+    else:
+        print("Failed to connect to the database after several attempts.")
+
+    #with app.app_context():
+        #db.create_all()
 
     return app
